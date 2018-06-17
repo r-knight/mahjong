e7d23b39d3db0fc1483f6c895100f9bec8e64153
ahfpca <ahfpca@gmail.com>
2018-06-16 22:38:09 -0700

    Hand classes start

---
+++ MeldTile.java
@@ -0,0 +1,7 @@
+import com.sjcdojo.mahjong.Player; // Temp
+import com.sjcdojo.mahjong.Tile; // Temp
+
+
+public class MeldTile extends Tile {
+    private Player prevPlayer; // not null if tile was claimed.
+}
